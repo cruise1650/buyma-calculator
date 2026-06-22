afd700cd8987bce8f76c4a3c98775c310a868375
HansoonPark <hansoonpark@HansoonParkui-Mac-mini.local>
2026-06-22 20:07:36 +0900

    Updates

---
+++ _next/static/UbLvT05_S_YMsd-BSsfEi/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/buyma-calculator//_next/:path+",
+        "destination": "/buyma-calculator/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
